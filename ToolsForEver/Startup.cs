c1c295a5555cbcafd413c9391c34edfd02282055
Teun Steggink <teun-steggink@hotmail.nl>
2018-12-10 22:13:15 +0100

    Add project files.

---
+++ ToolsForEver/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToolsForEver.Startup))]
+namespace ToolsForEver
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
